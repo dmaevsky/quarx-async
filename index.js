b9fd0ed000f38a4f853d26a729f4c12996e4d668
Dmitry Maevsky <dmitry.maevsky@gmail.com>
2021-05-25 23:02:21 +0900

    v0.1

---
+++ index.js
@@ -0,0 +1,112 @@
+import { createAtom, autorun } from 'quarx';
+import { conclude, inProgress, isFlow, isIterator } from 'conclure';
+
+const reactiveFlows = new WeakMap();
+
+function makeReactive(it, options = {}) {
+  const { name = 'makeReactive' } = options;
+
+  if (reactiveFlows.has(it)) {
+    return reactiveFlows.get(it);
+  }
+
+  const atom = createAtom(() => {
+    const originalNext = it.next;
+    const steps = [];
+
+    it.next = value => {
+      let result, error;
+      let step = steps.length;
+
+      const dispose = autorun(() => {
+        if (step !== steps.length) return atom.reportChanged();
+        try {
+          result = originalNext.call(it, value);
+
+          if (isIterator(result.value)) {
+            makeReactive(result.value).reportObserved();
+          }
+        }
+        catch (e) {
+          error = e;
+        }
+      });
+
+      steps.push(dispose);
+
+      if (error) throw error;
+      return result;
+    };
+
+    return () => {
+      it.next = originalNext;
+      for (let dispose of steps) dispose();
+    };
+  }, { name });
+
+  reactiveFlows.set(it, atom);
+  return atom;
+}
+
+export class Stale extends Error {};
+
+export function reactiveFlow(evaluate, options = {}) {
+  const {
+    name = 'reactiveFlow',
+    equals = (a, b) => a === b,
+  } = options;
+
+  let result, error, cancel;
+
+  function start() {
+    const stop = autorun(computation);
+
+    return () => {
+      if (cancel) cancel();
+      cancel = undefined;
+      stop();
+    };
+  }
+
+  const atom = createAtom(start, { name });
+
+  function set(e, r) {
+    error = e;
+    if (!error) {
+      if (equals(result, r)) return;
+      result = r;
+    }
+    atom.reportChanged();
+  }
+
+  function computation() {
+    try {
+      if (cancel) cancel();
+
+      const value = evaluate();
+
+      if (isIterator(value)) {
+        makeReactive(value).reportObserved();
+      }
+
+      cancel = conclude(value, set);
+
+      if (isFlow(value) && inProgress(value)) {
+        set(new Stale(name));
+      }
+    }
+    catch (err) {
+      set(err);
+    }
+  }
+
+  return {
+    get: () => {
+      if (!atom.reportObserved()) {
+        computation();
+      };
+      if (error) throw error;
+      return result;
+    }
+  };
+}
---
+++ tests/index.spec.js
@@ -0,0 +1,61 @@
+const test = require('ava');
+
+const { autorun, observable } = require('quarx');
+const { Stale, reactiveFlow } = require('../dist/index');
+
+test.cb('simple reactive promise', t => {
+  const logs = [];
+  const log = msg => logs.push(msg);
+
+  const obs = reactiveFlow(() => Promise.resolve('I am done'));
+
+  autorun(() => {
+    try {
+      log(obs.get());
+      t.deepEqual(logs, ['I am still running', 'I am done']);
+      t.end();
+    }
+    catch (e) {
+      if (e instanceof Stale) {
+        log('I am still running');
+      }
+      else console.error(e);
+    }
+  });
+});
+
+test.cb('multi-step flow', t => {
+  const logs = [];
+  const log = msg => logs.push(msg);
+
+  const b = observable.box(42);
+
+  function* flow(init) {
+    for (let i = 0; i < 5; i++) {
+      yield Promise.resolve();
+      init++;
+    }
+    return init + b.get();
+  }
+
+  const obs = reactiveFlow(() => flow(0));
+
+  autorun(() => {
+    try {
+      log(obs.get());
+
+      if (logs.length === 2) {
+        // Avoid circular dependency
+        Promise.resolve().then(() => b.set(142));
+      }
+      else if (logs.length === 4) {
+        t.deepEqual(logs, ['STALE', 47, 'STALE', 147]);
+        t.end();
+      }
+    }
+    catch (e) {
+      if (e instanceof Stale) log('STALE');
+      else console.error(e);
+    }
+  });
+})
